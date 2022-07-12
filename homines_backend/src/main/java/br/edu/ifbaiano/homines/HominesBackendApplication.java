492b88996f2bd1d4066087c99151269322d34b32
Aijalon Júnior <juniorsjs6@hotmail.com>
2022-07-12 16:49:08 -0300

    start project

---
+++ homines_backend/src/main/java/br/edu/ifbaiano/homines/HominesBackendApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifbaiano.homines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HominesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HominesBackendApplication.class, args);
+	}
+
+}
---
+++ homines_backend/src/test/java/br/edu/ifbaiano/homines/HominesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifbaiano.homines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HominesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
